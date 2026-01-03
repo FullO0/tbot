a35bcf0a2fef8b5aea44f4515b384f40175b4685
Christian Jacobus Van Der Walt <28047842@sun.ac.za>
2026-01-03 11:15:27 +0200

    Add boolean enum

---
+++ include/boolean.h
@@ -0,0 +1,16 @@
+/**
+ * @file    boolean.h
+ * @brief   Defintion of a Boolean value
+ * @author  CJ vd Walt (Christian@vanderwalts.net)
+ * @date    03/01/2026
+*/
+
+#ifndef BOOLEAN_H
+#define BOOLEAN_H
+
+typedef enum boolean {
+	FALSE = 0,
+	TRUE
+} boolean;
+
+#endif
