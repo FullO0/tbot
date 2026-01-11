f19ebed2729b4d0ec85c6830229e0e245a21aa0f
Christian Jacobus Van Der Walt <28047842@sun.ac.za>
2026-01-11 11:25:58 +0200

    Added commands to initialize and close the logfile

--- src/test_logger.c
+++ src/test_logger.c
@@ -28,29 +28,37 @@ int main(int argc, char *argv[])
 	if (!(in = fopen(filename, "r"))) goto error_file;
 
 	char level[10], msg[1024];
-	initLogFile();
 	while (1) {
-		if (fscanf(in, "%s %[^\n]", level, msg) != 2) goto error_input;
 
+		/* Level */
+		if (fscanf(in, "%s", level) != 1)   goto error_input;
+		if      (!(strcmp("INNIT", level))) initLogFile();
+		else if (!(strcmp("CLOSE", level))) closeLogFile();
+		else if (!(strcmp("END", level)))   goto exit;
+
+		/* message */
+		if (fscanf(in, "%[^\n]", msg) != 1) goto error_input;
 		if      (!(strcmp("DEBUG", level))) LOG_DEBUG("%s\n", msg);
 		else if (!(strcmp("INFO", level)))  LOG_INFO("%s\n", msg);
 		else if (!(strcmp("WARN", level)))  LOG_WARN("%s\n", msg);
 		else if (!(strcmp("ERROR", level))) LOG_ERROR("%s\n", msg);
-		else if (!(strcmp("END", level)))   goto exit;
 		else                                goto error_input;
 	}
 
 exit:
-	closeLogFile();
+	if (isOpen) {
+		LOG_WARN("Test file did not close Log file");
+		closeLogFile();
+	}
 	return EXIT_SUCCESS;
 
 error_input:
-	closeLogFile();
+	if (isOpen) closeLogFile();
 	perror("Please make sure file has valid input");
 	return EXIT_FAILURE;
 
 error_file:
-	closeLogFile();
+	if (isOpen) closeLogFile();
 	perror("Please Provide a valid input file as a argument");
 	return EXIT_FAILURE;
 }
